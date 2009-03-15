a2e32fe90cb720e0dd4d47141a7e3aa5de82ebb6
Your Name <Ealdwulf@googlemail.com>
2009-03-15 17:59:03 +0000

    Change to allow switching number class more easily. Set to float for now, as this appears to work once Beta() is implemented more sanely.

--- BBChop/source/numbers.py
+++ BBChop/source/numbers.py
@@ -15,19 +15,43 @@
 #    You should have received a copy of the GNU General Public License
 #    along with BBChop.  If not, see <http://www.gnu.org/licenses/>.
 
-import mpmath
-import math
-mpmath.mp.prec=200
-def const(str):
-    return mpmath.mpf(str)
+# define what kind of number object to use
 
-import pdb
+numberType = 'float'
+#numberType = 'mpmath'
+
+import math
+
+
+if numberType == 'mpmath':
+    import mpmath
+    mpmath.mp.prec=200
+
+    def const(str):
+        return mpmath.mpf(str)
+
+    power=mpmath.power
+
+elif numberType == 'float':
+    
+    def const(str):
+        return float(str)
+    
+    power=math.pow
+
+else:
+    raise "numberType set incorrectly\n"
+
+
 def log(x):
     return math.log(x)
 
+def exp(x):
+    return math.exp(x)
 
 def pow(a,b):
     if a==0:
         return 0
     else:
-        return mpmath.power(a,b)
+        return power(a,b)
+
