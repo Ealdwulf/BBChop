c128eea971d5e5c048078dfa1d627e325277e9a2
Your Name <Ealdwulf@googlemail.com>
2009-03-29 20:01:05 +0100

    Fix persistentMome to check that the memo dir exists, if not don't try to access it. Delete memo dir contents in perfgit.py fo make trial fair.

--- BBChop/source/persistentMemo.py
+++ BBChop/source/persistentMemo.py
@@ -10,31 +10,36 @@ hash=hashlib.sha1
 #state
 memoObjs=[]
 memoDir="/home/ealdwulf/.memoObjs"
-
-memoObjs=os.listdir(memoDir)
+if os.path.isdir(memoDir):
+    memoObjs=os.listdir(memoDir)
+else:
+    memoObjs=None
 
 class memo:
     def __init__(self,theClass):
         self.theClass=theClass
 
     def __call__(self,*args):
-        argsStr=pickle.dumps(args,pickleProto)
-        hobj=hash()
-        hobj.update(argsStr)
-        d=hobj.hexdigest()
-        fname=os.path.join(memoDir,d)
-
-        if d in memoObjs:
-            f=open(fname,"rb")
-            ret=pickle.load(f)
-            f.close()
-            return ret
+        if memoObjs is not None:
+            argsStr=pickle.dumps(args,pickleProto)
+            hobj=hash()
+            hobj.update(argsStr)
+            d=hobj.hexdigest()
+            fname=os.path.join(memoDir,d)
+    
+            if d in memoObjs:
+                f=open(fname,"rb")
+                ret=pickle.load(f)
+                f.close()
+                return ret
+            else:
+                ret=self.theClass(*args)
+                f=open(fname,"wb")
+                pickle.dump(ret,f,pickleProto)
+                f.close()
+                memoObjs.append(d)
+                return ret
         else:
-            ret=self.theClass(*args)
-            f=open(fname,"wb")
-            pickle.dump(ret,f,pickleProto)
-            f.close()
-            memoObjs.append(d)
-            return ret
+            return self.theClass(*args)
             
         
