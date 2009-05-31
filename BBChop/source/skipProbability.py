2ba6943b4c0ee2942e0b900e6dcf88fc55442563
Ealdwulf Wuffinga <Ealdwulf@googlemail.com>
2009-05-31 21:23:25 +0100

    Correct for double application of decay factor.

--- BBChop/source/skipProbability.py
+++ BBChop/source/skipProbability.py
@@ -8,29 +8,44 @@ def skipProbsSimple(skipped,dag):
 
 
 
-    
-def doGeoDecay(decayFactor,adjacent):
-    if len(adjacent)==0:
-        return numberType.zero
-    decayed = [a*decayFactor for a in adjacent]
-    return max(decayed)
+class doGeoDecay:
+    def __init__(self,decayFactor):
+        self.decayFactor=numberType.pow(decayFactor,numberType.const(0.5))
 
-def doArithDecay(decayFactor,adjacent):
-    if len(adjacent)==0:
-        return numberType.zero
-    decayed = [a+decayFactor for a in adjacent]
-    
-    return max(decayed)
+    def __call__(self,adjacent):
+        if len(adjacent)==0:
+            return numberType.zero
+        decayed = [a*self.decayFactor for a in adjacent]
+        return max(decayed)
+
+class doArithDecay:
+    def __init__(self,decayFactor):
+        self.decayFactor=decayFactor/numberType.const(2)
+
+    def __call__(self,adjacent):
+        if len(adjacent)==0:
+            return numberType.zero
+        decayed = [a+self.decayFactor for a in adjacent]
+        
+        return max(decayed)
 
 
 
 class skipProbsDecay:
     def __init__(self,decayFactor,decayFunc):
         self.decayFactor=decayFactor
-        self.combFunc = lambda adj: decayFunc(self.decayFactor,adj)
+
+        # NB, the dag comb() methods don't really work as a generic 
+        # propagation method. They assume their arguments are associative, so in fact the decay
+        # factor gets applied twice. The above classes have to work round this.
+
+
+        self.combFunc = decayFunc(self.decayFactor)
 
     def __call__(self,skipped,myDag):
         initial = skipProbsSimple(skipped,myDag)
+
+
 
         upto  = myDag.combUpto(initial,self.combFunc)
         after = myDag.combAfter(initial,self.combFunc)
